e65fb72c530fcedeedd2d8576388977d538cc3b6
Joshua Williams <jmw0426@hotmail.com>
2020-04-04 18:28:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,18 +4,26 @@ class String
 
   def sentence?
     if self.end_with?(".")
-    return true 
-  else 
-    return false
+      return true 
+    else 
+      return false
   end
 end
 
   def question?
-
+    if self.end_with?("?")
+     return true 
+    else 
+     return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+     return true 
+   else 
+     return false
+    end
   end
 
   def count_sentences
