df41141c66b845377c47de74b8adf9d4e96bba01
Joshua Williams <jmw0426@hotmail.com>
2020-04-04 18:51:03 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,6 +28,8 @@ end
 
   def count_sentences
 # if self.end_with?("!" || "." || "?")
+split_everything = self.strip.split(/(\.|\?|\!)/)
+split_everything.each_slice(2).to_a.map {|pair| pair.join}
 
 # self.split("!", ".", "?") = sentences 
 
