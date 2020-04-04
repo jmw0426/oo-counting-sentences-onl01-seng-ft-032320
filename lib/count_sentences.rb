b61688065dae880efdfef8b756ed7ca2c1f1d81d
Joshua Williams <jmw0426@hotmail.com>
2020-04-04 19:01:45 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,17 +27,8 @@ end
   end
 
   def count_sentences
-# if self.end_with?("!" || "." || "?")
-# sentences = self.strip.split(/(\.|\?|\!)/)
-# sentences = self.split(/\W+/)
-# sentences.count
-# split_everything.each_slice(2).to_a.map {|pair| pair.join}
-delimiters = ['!', '.', '?']
-sentences = self.split(Regexp.union(delimiters))
-sentences.count
-# self.split("!", ".", "?") = sentences 
-
-# puts str.split(",")
+    delimiters = ['!', '.', '?', '...']
+    sentences = self.split(Regexp.union(delimiters))
+    sentences.count
   end
-# end
 end
